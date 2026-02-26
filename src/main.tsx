edbb266ef554daf0db0447734d12ab6bafb23a83
Aditya4927 <168982976+Aditya4927@users.noreply.github.com>
2026-02-27 03:41:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
+import "./index.css";   // ⭐ VERY IMPORTANT
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
